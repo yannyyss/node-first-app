4147bb06957d57a2e3fcef0d3056002eee06d139
Yanely <yannyyss@hotmail.com>
2021-04-28 21:10:48 -0500

    Create a file and content with input

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const http = require('http')
-
+const fs = require('fs')
 
 // This function will run for each request reaches our server (at createServer)
 // function rqListener (req, res) {
@@ -9,10 +9,42 @@ const http = require('http')
 
 // This is the same function with diferent sintaxis for create a Server
 const server = http.createServer((req, res) => {
-    console.log("req.url", req.url, "req.method", req.method , "req.headers", req.headers)
-    // process.exit() // This funciton quit the server
+
+    const url = req.url
+    const method = req.method
+    
+    if (url  === '/') {
+        res.setHeader('Content-Type', 'text/html')
+        res.write('<html>')
+        res.write('<body>')
+        res.write('<form action="/message" method="POST"><input type="text" name="msg"><button type="submit">SEND</button></form') // this action redirect us to /message url
+        res.write('</body>')
+        res.write('</html>')
+        return res.end() // To not continue with the following lines we return res.end()
+    }
+
+    if (url  === '/message' && method === 'POST') {
+        const body = []
+        req.on('data', (chunk) => { // on method allow us to listen and event
+            console.log('chunk', chunk) // chunk with buffer
+            body.push(chunk)
+        }) 
+        req.on('end', () => { 
+            const parseBody = Buffer.concat(body).toString() // String of the data
+            const dataValue = parseBody.split('=')[1]
+            fs.writeFileSync('message.txt', dataValue)
+        })
+        res.statusCode = 302
+        res.setHeader('Location', '/')
+        return res.end()
+    }
     res.setHeader('Content-Type', 'text/html')
-    res.write('<html>Hello</html>')
+    res.write('<html>')
+    res.write('<body>')
+    res.write(`<div>${url} Page</div`)
+    res.write('</body>')
+    res.write('</html>')
+    // process.exit() // This funciton quit the server
 })
 
 // Listening to the server in an infinite loop (event loop)
